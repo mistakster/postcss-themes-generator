fdc1317c9c5b511ecb05d03bbbe4939fa278734c
Vladimir Kuznetsov <mistakster@gmail.com>
2018-04-23 15:12:53 +0500

    updated tests

--- test/plugin.spec.js
+++ test/plugin.spec.js
@@ -4,109 +4,139 @@ const plugin = require('../lib/index');
 
 const config = require('./config.json');
 
-function runProcessing(input) {
-	return postcss()
+function run(input, warningCount) {
+  return postcss()
     .use(plugin(config))
     .process(input, {from: undefined})
-		.then(result => {
-		  const css = result.css.toString();
+    .then(result => {
+      const css = result.css.toString();
 
-		  expect(css).toMatchSnapshot();
+      expect(css).toMatchSnapshot();
 
-			return result;
-		});
+      return result;
+    })
+    .then(result => {
+      expect(result.warnings()).toHaveLength(warningCount || 0);
+
+      return result;
+    });
 }
 
-function run(input, warningCount) {
-	return runProcessing(input)
-		.then(result => {
-			expect(result.warnings()).toHaveLength(warningCount || 0);
-
-			return result;
-		});
+it('should match "primary", "secondary" and "accent" modifiers', () => (
+  run(`
+.foo_primary {
+  color: #e54096;
 }
+.bar_secondary {
+  color: #0080c5;
+}
+.baz_accent {
+  color: #c1d730;
+}
+`)
+));
 
-it('should match "primary", "secondary" and "accent" modifiers', () => {
-	const input = '.foo_primary { color: #e54096 }' +
-		'.bar_secondary { color: #0080c5 }' +
-		'.baz_accent { color: #c1d730 }';
+it('should match modifiers at upper level', () => (
+  run(`
+.foo_primary .foo__test {
+  color: #e54096;
+}`
+)
+));
 
-	return run(input);
-});
+it('should match pseudo selector', () => (
+  run(`
+.foo_primary:hover {
+  color: #e54096;
+}
+`)
+));
 
-it('should match modifiers at upper level', () => {
-  const input = '.foo_primary .foo__test { color: #e54096 }';
+it('should warn on suspicious rule', () => (
+  run(`
+.foo_primary {
+  display: block;
+}
+`, 3)
+));
 
-  return run(input);
-});
+it('should match lighten 5% function', () => (
+  run(`
+.foo_primary {
+  color: #e856a2;
+}
+`)
+));
 
-it('should match pseudo selector', () => {
-	const input = '.foo_primary:hover { color: #e54096 }';
+it('should match lighten 15% function', () => (
+  run(`
+.foo_primary {
+  color: #ee83bb;
+}
+`)
+));
 
-	return run(input);
-});
+it('should match darken 5% function', () => (
+  run(`
+.foo_primary {
+  color: #e22a8a;
+}
+`)
+));
 
-it('should warn on suspicious rule', () => {
-	const input = '.foo_primary { display: block }';
+it('should match darken 15% function', () => (
+  run(`
+.foo_primary {
+  color: #bf1a70;
+}
+`)
+));
 
-	return run(input, 3);
-});
+it('should match color inside shortcut properties', () => (
+  run(`
+.foo_primary {
+  border: #bf1a70 1px solid;
+}
+`)
+));
 
-it('should match lighten 5% function', () => {
-	const input = '.foo_primary { color: #e856a2 }';
+it('should skip unknown values', () => (
+  run(`
+.foo_accent {
+  background-color: #c1d730;
+  border: none;
+}
+`)
+));
 
-	return run(input);
-});
+it('should process rgba() color', () => (
+  run(`
+.foo_accent {
+  background-color: rgba(193, 215, 48, 0.15);
+}
+`)
+));
 
-it('should match lighten 15% function', () => {
-	const input = '.foo_primary { color: #ee83bb }';
+it('should process rgba() color without spaces between commas', () => (
+  run(`
+.foo_accent {
+  background-color: rgba(193,215,48,0.15);
+}
+`)
+));
 
-	return run(input);
-});
+it('should process rgba() color in border', () => (
+  run(`
+.foo_accent {
+  border: 1px solid rgba(193, 215, 48, 0.15);
+}
+`)
+));
 
-it('should match darken 5% function', () => {
-	const input = '.foo_primary { color: #e22a8a }';
-
-	return run(input);
-});
-
-it('should match darken 15% function', () => {
-	const input = '.foo_primary { color: #bf1a70 }';
-
-	return run(input);
-});
-
-it('should match color inside shortcut properties', () => {
-	const input = '.foo_primary { border: #bf1a70 1px solid }';
-
-	return run(input);
-});
-it('should skip unknown values', () => {
-	const input = '.foo_accent { background-color: #c1d730; border: none; }';
-
-	return run(input);
-});
-
-it('should process rgba() color', () => {
-	const input = '.foo_accent { background-color: rgba(193, 215, 48, 0.15) }';
-
-	return run(input);
-});
-
-it('should process rgba() color without spaces between commas', () => {
-	const input = '.foo_accent { background-color: rgba(193,215,48,0.15) }';
-
-	return run(input);
-});
-
-it('should process rgba() color in border', () => {
-	const input = '.foo_accent { border: 1px solid rgba(193, 215, 48, 0.15) }';
-
-	return run(input);
-});
-
-it('should process rgba() color in border mirror order', () => {
-	const input = '.foo_accent { border: rgba(193, 215, 48, 0.15) 1px solid }';
-
-	return run(input);
-});
+it('should process rgba() color in border mirror order', () => (
+  run(`
+.foo_accent {
+  border: rgba(193, 215, 48, 0.15) 1px solid;
+}
+`)
+));
